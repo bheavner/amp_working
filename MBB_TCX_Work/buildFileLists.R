f87d03243475497b331bb8ebfb66703d252be293
Ben Heavner <bheavner@systemsbiology.org>
2015-04-07 16:58:16 -0700

    ongoing adventures with covariates tables

---
+++ MBB_TCX_Work/buildFileLists.R
@@ -0,0 +1,68 @@
+# goal: build list of files to include in data release Per Dr. Younkin, "We ran RNASeq on 278 mayo temporal cortex samples plus 10 samples from rush/broad = 288 samples." NOTE: of the 288 RNAseq samples, 10 are “sample-swap” samples from RUSH/Broad and 278 are Mayo temporal cortex samples.
+
+# so I want a list of the 10 samples (20 file names?) for RUSH-BROAD sample-swap and the 278 from MayoBrainBank_Dickson, BannerSunHealth_TomBeach
+
+# I'll use this for generating zip file for data processing and for making covariates files.
+
+library(synapseClient) # for synapse upload
+library(RCurl) # to grab google doc covariates files
+library(gdata) # to read .xlsx file
+library(dplyr) # for subsetting data
+
+#Login to Synapse using credentials saved in .synapseConfig file
+synapseLogin()
+
+# Grab existing info for inspection and assembly (James organized this across subdirectories in syn2924444)
+
+#RNASeq_fullSampleInformation_NET.xlsx syn3163262
+# has RNASubjectID, RNAId, Source, tissue, final diagnosis, RIN, unfiltered age, sex, and Braak
+fullSampleInformation <- synGet("syn3163262")
+fullSampleInformationFilePath <- getFileLocation(fullSampleInformation)
+fullSample <- read.xls(fullSampleInformationFilePath, sheet = 1, header = TRUE, stringsAsFactors = FALSE)
+
+#U01_288_AUT_TCx_RNAseq_Covars-Drives_02-06-2015_1447.xlsx syn3523879
+# includes SubjectID, SampleID, seq Run ID, DriveA_FileName1, DriveA_FileName2, DriveB_FileName1, and DriveB_Filename2
+rerunSamples  <- synGet("syn3523879")
+rerunSamplesFilePath <- getFileLocation(rerunSamples)
+rerun <- read.xls(rerunSamplesFilePath, sheet = 1, header = TRUE, stringsAsFactors = FALSE)
+
+# get file names for RUSH-BROAD samples. Some have been rerun. Which ones?
+
+rushBroadSamples <- fullSample[fullSample$Source == "RUSH-BROAD", "RNASubjectId"]
+length(rushBroadSamples) #10
+length(intersect(rushBroadSamples, rerun$SubjectID)) #10 - so all of them!
+
+rushBroadFile1 <- rerun[fullSample$Source == "RUSH-BROAD", "DriveA_FileName1"]
+rushBroadFile2 <- rerun[fullSample$Source == "RUSH-BROAD", "DriveA_FileName2"]
+rushBroadFile3 <- rerun[fullSample$Source == "RUSH-BROAD", "DriveB_FileName1"]
+rushBroadFile4 <- rerun[fullSample$Source == "RUSH-BROAD", "DriveB_FileName2"]
+
+rushBroadFiles <- c(rushBroadFile1[rushBroadFile1 != "NULL"], 
+                    rushBroadFile2[rushBroadFile2 != "NULL"], 
+                    rushBroadFile3[rushBroadFile3 != "NULL"], 
+                    rushBroadFile4[rushBroadFile4 != "NULL"])
+
+# next, build list of files from MayoBrainBank_Dickson and BannerSunHealth_TomBeach, including the most recent if rerun.
+MBBSamples <- (rerun$Source == "BannerSunHealth_TomBeach" | rerun$Source == "MayoBrainBank_Dickson")
+MBBSampleIds <- rerun[MBBSamples, "SubjectID"]
+length(MBBSampleIds) #278. Yay!
+
+MBBFile1 <- rerun[MBBSamples, "DriveA_FileName1"]
+MBBFile2 <- rerun[MBBSamples, "DriveA_FileName2"]
+MBBFile3 <- rerun[MBBSamples, "DriveB_FileName1"]
+MBBFile4 <- rerun[MBBSamples, "DriveB_FileName2"]
+
+MBBFiles <- c(MBBFile1[MBBFile1 != "NULL"],
+              MBBFile2[MBBFile2 != "NULL"], 
+              MBBFile3[MBBFile3 != "NULL"], 
+              MBBFile4[MBBFile4 != "NULL"])
+
+length(MBBFiles) #568... should be 278 * 2 = 556
+#it turns out there are 6 files on both drive A and drive B, for the following subject IDs:
+# NA08-370
+# NA05-054
+# NA01-233
+# NA03-163
+# 00-32
+# 10-20
+length(unique(MBBFiles)) #556 - that's better. So that's the set of files I want to include in my .tar file from snapr output and samples for the MBB TCX covariates file.
--- MBB_TCX_Work/makeMBBTCXCovariates.R
+++ MBB_TCX_Work/makeMBBTCXCovariates.R
@@ -1,9 +1,10 @@
-# Goal: build MBB TCX covariates table.
+# Goal: build MBB TCX covariates table. Per Dr. Younkin, "We ran RNASeq on 278 mayo temporal cortex samples plus 10 samples from rush/broad = 288 samples." NOTE: of the 288 RNAseq samples, 10 are “sample-swap” samples from RUSH/Broad and 278 are Mayo temporal cortex samples
+
 # This will go in the folder at syn3163039
 
 library(synapseClient) # for synapse upload
 library(RCurl) # to grab google doc covariates files
-require(gdata) # to read .xlsx file - http://www.r-bloggers.com/read-excel-files-from-r/
+library(gdata) # to read .xlsx file -  install.packages("gdata")
 
 source("uploadToSynapse.R") # to annotate and upload file
 
@@ -13,54 +14,47 @@ synapseLogin()
 # Grab existing info for inspection and assembly (James organized this across subdirectories in syn2924444)
 
 #RNASeq_fullSampleInformation_NET.xlsx syn3163262
+# has RNASubjectID, RNAId, Source, tissue, final diagnosis, RIN, unfiltered age, sex, and Braak
+fullSampleInformation <- synGet("syn3163262")
+fullSampleInformationFilePath <- getFileLocation(fullSampleInformation)
+fullSample <- read.xls(fullSampleInformationFilePath, sheet = 1, header = TRUE, stringsAsFactors = FALSE)
+
 #mayo_tcx_rnaseq_clinical_vars.txt syn3163736
+# censored: has participant_id, age_at_last_assessment for some samples, sex, Braak
+clinicalVars <- synGet("syn3163736")
+clinicalVarsFilePath <- getFileLocation(clinicalVars)
+clinical <- read.table(clinicalVarsFilePath, header = TRUE, stringsAsFactors = FALSE)
+
 #mayo_tcx_rnaseq_tech_vars.txt syn3163738 
+# has RNASubjectID, RNAId, Source, tissue, RIN
+techVars <- synGet("syn3163738")
+techVarsFilePath <- getFileLocation(techVars)
+tech <- read.table(techVarsFilePath, header = TRUE, stringsAsFactors = FALSE)
+
 #mayo_tcx_rnaseq_sample_groups.txt syn3163739
+# may not need
+sampleGroups <- synGet("syn3163739")
+sampleGroupsFilePath <- getFileLocation(sampleGroups)
+groups <- read.table(sampleGroupsFilePath, header = TRUE, stringsAsFactors = FALSE)
+#Error in scan(file, what, nmax, sep, dec, quote, skip, nlines, na.strings,  : line 19 did not have 6 elements
 
-# and I'll need to work with the samples that were re-run for higher readcounts
-# https://docs.google.com/spreadsheets/d/1IN2Sgv517UMBnjYqcnIcv63KL-K42QCXl4P88XjwecY/
+#U01_288_AUT_TCx_RNAseq_Covars-Drives_02-06-2015_1447.xlsx syn3523879
+# THIS IS WHAT I WANT TO USE FOR EVERYTHING.
+# includes SubjectID, SampleID, seq Run ID, DriveA_FileName1, DriveA_FileName2, DriveB_FileName1, and DriveB_Filename2
+rerunSamples  <- synGet("syn3523879")
+rerunSamplesFilePath <- getFileLocation(rerunSamples)
+rerun <- read.xls(rerunSamplesFilePath, sheet = 2, header = TRUE, stringsAsFactors = FALSE)
 
-url1 <- getURL("https://docs.google.com/spreadsheets/d/1IN2Sgv517UMBnjYqcnIcv63KL-K42QCXl4P88XjwecY/export?format=csv")
-reRunSamples <- read.csv(textConnection(url1))
+# the fullSampleInformation_NET.xlsx includes data from 3 sources: MayoBrainBank_Dickson, BannerSunHealth_TomBeach, and RUSH_BROAD. I need to generate different covariates files for the RUSH_BROAD and the other samples. RUSH_BROAD under syn3388564, others under syn3163039.
 
-metaDataFileSynapseID <- c('syn3163262', 'syn3163736', 'syn3163738', 'syn3163739')
+# Begin by building lists of sample IDs to include in each of the 2 covariates files (RUSH_BROAD and MBBTCX)
 
-for (metaDataFile in metaDataFileSynapseID) {
-  message("Grabbing ", metaDataFile)
-  
-  # Download file from Synapse
-  originalCountFile <- synGet(mergedCountFile)
-  
-  # unzip file and load for processing
-  localFilePath <- getFileLocation(originalCountFile)
-  
-  if(!file.exists(substr(localFilePath, 1, nchar(localFilePath) - 3))) {
-    gunzip(localFilePath)
-  }
-  
-  localFilePath <- sub('.gz', '', localFilePath) #trim the .gz suffix
-  
-  Counts <- read.table(localFilePath, header = TRUE)
-  
+rushBroadCovariates  <- data.frame()
+rushBroadCovariates$Samples <- fullSample[fullSample$Source == "RUSH-BROAD", "RNASubjectId"]
 
 
 
-
-
-# better to use readxls
-
-# excel summary files on google drive - permissions are set to "anyone with link can view"
-
-# file “20141209 Taner mouse-mRNA Summary.xlsx”  includes Mouse_ID, Seq.Run.ID, Lane.Number, Clusters, RLIMS.ID; part of the Raw_RNAseq_file_name (based on Seq Run ID); it's at 
-url1 <- getURL("https://docs.google.com/spreadsheets/d/1IQnJheILYLsUgWbwPT7Qmrmi-JngK0R3HTAJUfBYRUg/export?format=csv")
-
-# “Copy of APP Randomization 9-16-14.xlsx” includes mouse_ID, Line == Experiment, RIN, Genotype (to transform), Sex, age in months; it's at
-url2 <- getURL("https://docs.google.com/spreadsheets/d/1X_QRh-xw8q3lZ8IOosHdydxUsoP2Ucgxbfbk0FzMJx4/export?format=csv")
-
-rawCovariates1 <- read.csv(textConnection(url1))
-rawCovariates2 <- read.csv(textConnection(url2))
-
-#remove first row ov rawCovariates2 (all blanks)
+#remove first row of rawCovariates2 (all blanks)
 rawCovariates2 <- rawCovariates2[2:129,]
 
 #rawCovariates1 has 188 observations. Those with rawCovariates1$Request.ID == "REQ-000000002442" overlaps the 128 observations in rawCovariates2. I'm not sure what the others are.
