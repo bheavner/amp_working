73cfa1bad4ff7af2ddd5e4426111d6804afe0dd3
Ben Heavner <bheavner@systemsbiology.org>
2015-04-09 14:49:49 -0700

    makeAPPCovariates is shaping up

--- APP work/makeAPPCovariates.R
+++ APP work/makeAPPCovariates.R
@@ -2,14 +2,14 @@
 # NOTE: I expect 128 samples in this group
 # This will go in the folder at syn3435792
 
-library(synapseClient) # for synapse upload
+library(synapseClient) # for synapse data exchange
 library(RCurl) # to grab google doc covariates files
 source("uploadToSynapse.R")
 
 #Login to Synapse using credentials saved in .synapseConfig file
 synapseLogin()
 
-# Tau covariates are:"Mouse_ID" "Experiment" "RIN" "Genotype" "Sex" "Age_months" "RLIMS.ID" "Seq.Run.ID" "Lane.Number" "Clusters" "Raw_RNAseq_file_name"
+# Covariates will be:"Mouse_ID" "Experiment" "RIN" "Genotype" "Sex" "Age_months" "RLIMS.ID" "Seq.Run.ID" "Lane.Number" "Clusters" "BAM_file_name"
 
 # excel summary files on google drive - permissions are set to "anyone with link can view"
 
@@ -22,7 +22,7 @@ url2 <- getURL("https://docs.google.com/spreadsheets/d/1X_QRh-xw8q3lZ8IOosHdydxU
 rawCovariates1 <- read.csv(textConnection(url1))
 rawCovariates2 <- read.csv(textConnection(url2))
 
-#remove first row ov rawCovariates2 (all blanks)
+#remove first row of rawCovariates2 (all blanks)
 rawCovariates2 <- rawCovariates2[2:129,]
 
 #rawCovariates1 has 188 observations. Those with rawCovariates1$Request.ID == "REQ-000000002442" overlaps the 128 observations in rawCovariates2. I'm not sure what the others are.
@@ -52,8 +52,31 @@ APPCovariates <- cbind(APPCovariates, "Clusters" =
                          rawCovariates1$Clusters[match(APPCovariates$Mouse_ID, 
                                                          rawCovariates1$Sample.Name)])
 
-#TODO: filenames - do I need them? These are fastq files, so theree are two per sample - R1 and R2...
-#APPCovariates <- cbind(APPCovariates, "Raw_RNAseq_file_name")
+## add bam filenames - generated with makeFileList, at syn3539805
+bamFileListObject <- synGet('syn3539805')
+localFilePath <- getFileLocation(bamFileListObject)
+bamFileList <- read.table(localFilePath, header = FALSE, stringsAsFactors = FALSE)
+bamFileList <- bamFileList$V1
+
+# NOTE: Sample 181710 was rerun for technical reasons. I want to discard the first run, 181710.FCC5PJFACXX_L8_IGTGAAA.snap.bam, and keep the later one, run in lane 2: 181710.FCC5PVPACXX_L2_IGTGAAA.snap.bam
+
+remove <- "APP_Samples/snapr/181710.FCC5PJFACXX_L8_IGTGAAA.snap.bam"
+bamFileList <- bamFileList [! bamFileList %in% remove]
+
+# get sample ID from file list:
+bamFileList <- gsub("APP_Samples/snapr/", "", bamFileList) # strip APP_Samples/snapr/ from beginning
+bamFiles <- gsub(".snap.bam", "", bamFileList) #strip .snap.bam from end
+bamFiles <- gsub("\\..+", "", bamFiles) # strip everything after a period
+
+# modify ids to be like APPCovariates$Mouse_ID
+bamFiles <- gsub("DRB_", "DRB#", bamFiles) # replace DRB_ with DRB#
+bamFiles <- gsub("_", " ", bamFiles) # replace _ with space
+
+# now, build column of bam Files in order corresponding to covariates sample list by matching bamFiles to APPCovariates$Mouse_ID
+
+APPCovariates <- cbind(APPCovariates, 
+                       "BAM_file_name" = bamFileList[order(match(bamFiles, 
+                                                                 APPCovariates$Mouse_ID))])
 
 
 ## Reformat values for consistency Tau covariates
